3f7e1f797e2ef5eaaf2090536dd1b58bc1624849
Richard Walsh <richwalsh2@googlemail.com>
2022-04-03 19:18:14 +0100

    continue js

--- script.js
+++ script.js
@@ -11,15 +11,45 @@ console.log(document.querySelectorAll('[data-nav-item="one"]'));
 console.log(test);
 console.log(elements.indicator);
 
+const getCurrentIndicatorPosition = () => {
+  return elements.indicator.getBoundingClientRect();
+};
+
 const positionIndicator = (event) => {
   const element = document.querySelector(
     `[data-nav-item="${event.target.dataset.navItem}"]`
   );
   console.log('hello');
   console.log(element.getBoundingClientRect());
+
+  const nav = document.querySelector('.nav').getBoundingClientRect();
+
+  const indicatorPostion = getCurrentIndicatorPosition();
   const elementPosition = element.getBoundingClientRect();
 
-  elements.indicator.style.left = `${elementPosition.left}px`;
+  console.log('indicator', indicatorPostion.left);
+  console.log('element', elementPosition.left);
+
+  const left = elementPosition.left - nav.left;
+  console.log(left);
+
+  //   centers the indicator over the nav item
+  const setIndicatorPosition =
+    elementPosition.width / 2 - indicatorPostion.width / 2 + left;
+  //   const setIndicatorPosition = left;
+  console.log(indicatorPostion);
+
+  //   elements.indicator.style.left = `${setIndicatorPosition}px`;
+  elements.indicator.style.left = `${setIndicatorPosition}px`;
+
+  console.log(element.querySelector('ion-icon'));
+
+  const childIcon = element.querySelector('ion-icon');
+  const childText = element.querySelector('p');
+
+  childIcon.style.top = '-50px';
+  childText.style.top = '-5px';
+  childText.style.opacity = '1';
 };
 
 test.forEach((element) =>
